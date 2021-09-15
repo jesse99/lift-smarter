32e6622f701958addad2688a1e06ad60bf693270
Jesse Vorisek <jesse.vorisek@gmail.com>
2021-09-14 18:56:57 -0700

    Added an architecture note

--- liftsmarter/Model/Model.swift
+++ liftsmarter/Model/Model.swift
@@ -4,7 +4,12 @@ import Foundation
 
 /// Encapsulates all the state associated with the application. This layer has just data and simple accessors; actual logic
 /// belongs in the logic layer.
-class Model {               // I think the nested data will be ObservableObject's instead of the model itself
+class Model {
+    // State management is a bit of a pain with SwiftUI. It's normally best if state is all structs so that
+    // ObservableObject's auto-update as their fields change. But life is way easier to handle mutation for
+    // shared objects with classes. So that's what we do with the caveat that when we mutate objects embedded
+    // within arrays we need to explicitly call objectWillChange.send to inform views that the state changed
+    // (this is the "nested ObservableObject" problem).
     var program: Program
     var logs: Logs
     // TODO: add history, fixed weight sets, userNotes
